a997f4ad3ca0a49966bd27323c5699b670cbe5d0
Utkarsh Narain Srivastava <utkarshnarain@gmail.com>
2016-03-06 18:13:10 +0530

    Added perf. optimized splashscreen

---
+++ app/src/main/java/faclon/sensorremoteclient/Splash.java
@@ -0,0 +1,19 @@
+package faclon.sensorremoteclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Utkarsh on 06-Mar-16.
+ */
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
